672b6fa688a0de7f6429d83ff901b320b3860f81
Valentina Restrepo <73320777+ValentinaRestrepo2@users.noreply.github.com>
2025-11-18 22:59:20 -0500

    Creación modelo logístico

---
+++ Regresion_Logistica.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+df = pd.read_csv('Loan_Eligibility_1200.csv')
+
+nuevas_columnas = [
+    'ID_Cliente', 'Genero', 'Casado', 'Dependientes', 'Educacion', 'Independiente',
+    'Ingreso_Solicitante', 'Ingreso_CoSolicitante', 'Monto_Prestamo',
+    'Plazo_Prestamo', 'Historial_Credito', 'Zona_Propiedad', 'Estado_Prestamo'
+]
+df.columns = nuevas_columnas
+
+print("--- Información del Dataset ---")
+print(df.info())
+print(df.head())
+
+# Limpieza y Transformación de Datos
+df = df.drop('ID_Cliente', axis=1)
+# Genero: Male -> 1, Female -> 0
+df['Genero'] = df['Genero'].map({'Male': 1, 'Female': 0})
+# Casado: Yes -> 1, No -> 0
+df['Casado'] = df['Casado'].map({'Yes': 1, 'No': 0})
+# Educacion: Graduate -> 1, Not Graduate -> 0
+df['Educacion'] = df['Educacion'].map({'Graduate': 1, 'Not Graduate': 0})
+# Independiente: Yes -> 1, No -> 0
+df['Independiente'] = df['Independiente'].map({'Yes': 1, 'No': 0})
+# Estado_Prestamo (Variable Objetivo): Y -> 1, N -> 0
+df['Estado_Prestamo'] = df['Estado_Prestamo'].map({'Y': 1, 'N': 0})
+# Zona_Propiedad: Asignamos valores numéricos arbitrarios para poder procesarlos
+# Urban -> 2, Semiurban -> 1, Rural -> 0
+df['Zona_Propiedad'] = df['Zona_Propiedad'].map({'Urban': 2, 'Semiurban': 1, 'Rural': 0})
+
+# Validar valores nulos
+print(df.isna().sum())
+
+# Definición de variables X (Predictoras) e y (Objetivo)
+X = df.drop('Estado_Prestamo', axis=1)
+y = df['Estado_Prestamo']
+
+# División del dataset en entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Entrenamiento del modelo
+modelo = LogisticRegression(max_iter=1000) # max_iter aumentado para asegurar convergencia
+modelo.fit(X_train, y_train)
+
+# Realizar predicciones
+y_pred = modelo.predict(X_test)
+
+# Evaluación del modelo
+print(f"Accuracy- Precisión del Modelo: {accuracy_score(y_test, y_pred):.2f}")
+
+print("\n--- Matriz de Confusión ---")
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# Visualización de la matriz de confusión
+etiquetas = ['No le presta', 'Si le presta']
+plt.figure(figsize=(6,4))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', 
+            xticklabels=etiquetas, 
+            yticklabels=etiquetas)
+plt.title('Matriz de Confusión')
+plt.ylabel('Real')
+plt.xlabel('Predicción')
+plt.savefig('confusion_matrix_loan.png')
+
+print("\n--- Reporte de Clasificación ---")
+print(classification_report(y_test, y_pred, target_names=etiquetas))
